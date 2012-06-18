1f5e3b941f7e2b78a7914ad243b62931a15abe83
Ian Johnson <ianjohnson2000@gmail.com>
2012-06-18 21:58:09 +0100

    Update tests to have_selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static Pages" do
     it "should have the content 'Sample App'" do
       
       visit '/static_pages/home'
-      page.should have_content ('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
       
     end
   end
@@ -17,7 +17,17 @@ describe "Static Pages" do
     it "should have the content 'Help'" do
       
       visit '/static_pages/help'
-      page.should have_content ('Help')
+      page.should have_selector('h1', :text => 'Help')
+      
+    end
+  end
+  
+  describe "About Page" do 
+    
+    it "should have the content 'About Us'" do
+      
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
       
     end
   end
