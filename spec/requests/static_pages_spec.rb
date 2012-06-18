d9d159aae74145ed9a15fcab83d0766eb8dec2fb
Ian Johnson <ianjohnson2000@gmail.com>
2012-06-18 22:04:19 +0100

    Update rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,15 @@ describe "Static Pages" do
       page.should have_selector('h1', :text => 'Sample App')
       
     end
+    
+    it "should have the right title" do
+      
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+                                :text => "Ruby on Rails Sample App | Home")
+                                
+    end
+    
   end
 
   describe "Help Page" do 
@@ -20,6 +29,14 @@ describe "Static Pages" do
       page.should have_selector('h1', :text => 'Help')
       
     end
+    
+    it "should have the right title" do
+      
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+                                :text => "Ruby on Rails Sample App | Help")
+                                
+    end
   end
   
   describe "About Page" do 
@@ -29,6 +46,14 @@ describe "Static Pages" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
       
+    end
+    
+    it "should have the right title" do
+      
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+                                :text => "Ruby on Rails Sample App | About Us")
+                                
     end
   end
 
