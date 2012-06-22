f00118b498f46f4b5a5d3e07304dd5b23dad9724
Ian Johnson <ianjohnson2000@gmail.com>
2012-06-22 20:50:12 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
   
+  # Static Page Routes
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
   match '/about', to: 'static_pages#about'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 
 describe "Static Pages" do
   
-  let(:title) { "Ruby on Rails Sample App" }
   subject { page }
 
   describe "Home Page" do 
     
     before { visit root_path }
     it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: "#{title}") }
-    it { should_not have_selector('title', text: "#{title} | Home") }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: full_title('Home')) }
     
   end
 
@@ -18,7 +17,7 @@ describe "Static Pages" do
     
     before { visit help_path }
     it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: "#{title} | Help") }
+    it { should have_selector('title', text: full_title('Help')) }
     
   end
   
@@ -26,7 +25,7 @@ describe "Static Pages" do
     
     before { visit about_path }
     it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: "#{title} | About Us") }
+    it { should have_selector('title', text: full_title('About Us')) }
     
   end
 
@@ -34,15 +33,28 @@ describe "Static Pages" do
     
     before { visit contact_path }
     it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: "#{title} | Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
     
-    it "should have the right links on the layout" do
-      
-      visit root_path
-      click_link "sample app"
-      page.should have_selector('h1', text: 'Sample App')
-      
-    end
+  end
+
+  it "should have the right links on the layout" do
+    
+    visit root_path
+    click_link "sample app"
+    page.should have_selector('title', text: full_title(''))
+    
+    click_link "About"
+    page.should have_selector('title', text: full_title('About Us'))
+    
+    click_link "Help"
+    page.should have_selector('title', text: full_title('Help'))
+    
+    click_link "Contact"
+    page.should have_selector('title', text: full_title('Contact'))
+    
+    click_link "Home"
+    click_link "Sign Up Now!"
+    page.should have_selector('title', text: full_title('Sign Up'))
     
   end
 
